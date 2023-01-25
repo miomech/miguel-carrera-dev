e04c33a9567c7e53b4fa81d5c8bfeff475cfe7e3
miomech <29011382+miomech@users.noreply.github.com>
2023-01-24 23:25:28 -0600

    setup model

--- src/components/three/Experience.jsx
+++ src/components/three/Experience.jsx
@@ -1,45 +1,54 @@
 import {ContactShadows, Environment, Float, PresentationControls, Text} from '@react-three/drei'
 import Model from "@/components/three/Model";
+import {Canvas} from "@react-three/fiber";
 
 export default function Experience() {
 
-
     return (
         <>
-            <Environment preset='city'/>
-            <color/>
-            {/* Requires css to fix the mobile issue */}
-            {/* Presentation controls uses "spring" under the hood */}
-            <PresentationControls
-                global
-                rotation={[0.13, 0.1, 0]}
-                polar={[-0.4, 0.2]}
-                azimuth={[-1, 0.75]}
-                config={{mass: 2, tension: 400}}
-                snap={{mass: 4, tension: 400}}
-
+            <Canvas
+                camera={{
+                    fov: 45,
+                    near: 0.1,
+                    far: 2000,
+                    position: [-3, 1.5, 4]
+                }}
             >
-                <Float rotationIntensity={0.4}>
-                    <Model/>
-                    <Text
-                        font='./bangers-v20-latin-regular.woff'
-                        fontSize={1}
-                        position={[2, 0.75, 0.75]}
-                        rotation-y={-1.25}
-                        maxWidth={2}
-                        textAlign='center'
-                    >
-                        Bruno Simon
-                    </Text>
-                </Float>
-            </PresentationControls>
 
-            <ContactShadows
-                position-y={-1.4}
-                opacity={0.4}
-                scale={5}
-                blur={2.4}
-            />
+                <Environment preset='city'/>
+                {/* Requires css to fix the mobile issue */}
+                {/* Presentation controls uses "spring" under the hood */}
+                <PresentationControls
+                    global
+                    rotation={[0.13, 0.1, 0]}
+                    polar={[-0.4, 0.2]}
+                    azimuth={[-1, 0.75]}
+                    config={{mass: 2, tension: 400}}
+                    snap={{mass: 4, tension: 400}}
+
+                >
+                    <Float rotationIntensity={0.4}>
+                        <Model/>
+                        <Text
+                            font='./bangers-v20-latin-regular.woff'
+                            fontSize={1}
+                            position={[2, 0.75, 0.75]}
+                            rotation-y={-1.25}
+                            maxWidth={2}
+                            textAlign='center'
+                        >
+                            Hello!
+                        </Text>
+                    </Float>
+                </PresentationControls>
+
+                <ContactShadows
+                    position-y={-1.4}
+                    opacity={0.4}
+                    scale={5}
+                    blur={2.4}
+                />
+            </Canvas>
         </>
     )
 }
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -8,48 +8,40 @@ import {Canvas} from "@react-three/fiber";
 
 export default function Home() {
     return (<>
-        <Head>
-            <title>
-                Miguel Carrera - Software developer, designer
-            </title>
-            <meta
-                name="description"
-                content="Name's Miguel Carrera, I'm a software developer and designer!"
-            />
-        </Head>
-        <Container className="mt-1 sm:mt-20">
-            <div>
-                <div className="max-w-2xl">
-                    <h1 className="text-4xl font-bold tracking-tight text-primary-800 dark:text-neutral-100 sm:text-5xl ">
-                        Fullstack Web developer, designer, and maker.
-                    </h1>
+            <Head>
+                <title>
+                    Miguel Carrera - Software developer, designer
+                </title>
+                <meta
+                    name="description"
+                    content="Name's Miguel Carrera, I'm a software developer and designer!"
+                />
+            </Head>
+            <Container className="mt-1 sm:mt-20">
+                <div>
+                    <div className="max-w-2xl">
+                        <h1 className="text-4xl font-bold tracking-tight text-primary-800 dark:text-neutral-100 sm:text-5xl ">
+                            Fullstack Web developer, designer, and maker.
+                        </h1>
+                    </div>
+                    <p className="mt-6 text-base text-primary-500 dark:text-neutral-200">
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam asperiores debitis magnam
+                        omnis, porro voluptate. Doloremque ratione, temporibus. Harum hic, minus? At aut corporis
+                        deleniti fugiat minima recusandae repellat repudiandae.
+                    </p>
+                    <Button variant='primary' className='mt-5' href='/about'> Learn More </Button>
+                    <div className="mt-6 flex gap-6">
+                        <SocialLink
+                            href="https://github.com/miomech"
+                            aria-label="View my GitHub"
+                            icon={GitHubIcon}
+                        />
+                    </div>
                 </div>
-                <p className="mt-6 text-base text-primary-500 dark:text-neutral-200">
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam asperiores debitis magnam
-                    omnis, porro voluptate. Doloremque ratione, temporibus. Harum hic, minus? At aut corporis
-                    deleniti fugiat minima recusandae repellat repudiandae.
-                </p>
-                <Button variant='primary' className='mt-5' href='/about'> Learn More </Button>
-                <div className="mt-6 flex gap-6">
-                    <SocialLink
-                        href="https://github.com/miomech"
-                        aria-label="View my GitHub"
-                        icon={GitHubIcon}
-                    />
-                </div>
-            </div>
-        </Container>
-        <Container className="mt-5 md:mt-10">
-            <Canvas
-                camera={{
-                    fov: 45,
-                    near: 0.1,
-                    far: 2000,
-                    position: [-3, 1.5, 4]
-                }}
-            >
+            </Container>
+            <div className='mt-5 md:mt-10 h-[80vh]'>
                 <Experience/>
-            </Canvas>
-        </Container>
-    </>)
+            </div>
+        </>
+    )
 }
